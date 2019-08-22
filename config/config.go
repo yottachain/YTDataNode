da680f8b40867936244e6e5717143e117eccd66e
dp524856 <1456887752@qq.com>
2019-08-22 15:06:03 +0800

    merge peie

--- config/config.go
+++ config/config.go
@@ -32,7 +32,7 @@ type Config struct {
 	APIListen  string     `json:"APIListen"`
 	IndexID    uint32     `json:"IndexID"`
 	PoolID     string     `json:"PoolID"`
-	MaxConn    int        `json:"maxConn"`
+	MaxConn    int        `json:"MaxConn"`
 	*ytfsOpts.Options
 }
 
