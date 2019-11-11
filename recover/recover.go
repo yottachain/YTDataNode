8492b0dde923953daff670cda7aefedf29bb9eda
dp524856 <1456887752@qq.com>
2019-11-11 15:21:16 +0800

    diskqueue

--- recover/recover.go
+++ recover/recover.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"github.com/gogo/protobuf/proto"
+	"github.com/kkdai/diskqueue"
 	"github.com/klauspost/reedsolomon"
 	"github.com/mr-tron/base58/base58"
 	"github.com/yottachain/YTDataNode/host"
@@ -12,49 +13,52 @@ import (
 	"github.com/yottachain/YTDataNode/util"
 	"github.com/yottachain/YTFS"
 	"github.com/yottachain/YTFS/common"
+	"path"
 	"sync"
 	"time"
 )
 
 type RecoverEngine struct {
-	reedsolomon.Encoder
-	host *host.Host
-	ytfs *ytfs.YTFS
+	host  *host.Host
+	ytfs  *ytfs.YTFS
+	queue diskqueue.WorkQueue
 }
 
 func New(hst *host.Host, yt *ytfs.YTFS) (*RecoverEngine, error) {
 	var re = new(RecoverEngine)
-	encoder, err := reedsolomon.New(128, 32)
-	if err != nil {
-		return nil, err
-	}
 	re.host = hst
 	re.ytfs = yt
-	re.Encoder = encoder
+	re.queue = diskqueue.NewDiskqueue("taskQueue", util.GetYTFSPath())
 	return re, nil
 }
 
 func (re *RecoverEngine) recoverShard(description *message.TaskDescription) error {
-	var shards [][]byte = make([][]byte, 160)
+	var size = len(description.Hashs)
+	var shards [][]byte = make([][]byte, size)
+	encoder, err := reedsolomon.New(size-int(description.ParityShardCount), int(description.ParityShardCount))
+	if err != nil {
+		return err
+	}
 	var wg = sync.WaitGroup{}
 	var number int
 	wg.Add(len(description.Locations))
+	log.Printf("[recover:%s]recover start %d\n", base58.Encode(description.Id), size)
 	for k, v := range description.Locations {
 		go func(k int, v *message.P2PLocation) {
 			defer wg.Done()
 			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 			defer cancel()
-			shard, err := re.getShard(ctx, v.NodeId, v.Addrs, description.Hashs[k], &number)
+			shard, err := re.getShard(ctx, v.NodeId, base58.Encode(description.Id), v.Addrs, description.Hashs[k], &number)
 			if err == nil {
 				shards[k] = shard
 			} else {
-				log.Printf("[recover:%s]error:%s\n", base58.Encode(description.Id), err.Error())
+				log.Printf("[recover:%s]error:%s, %v, %s\n", base58.Encode(description.Id), err.Error(), v.Addrs, v.NodeId)
 			}
 		}(k, v)
 	}
 	wg.Wait()
 	shards[description.RecoverId] = nil
-	err := re.Encoder.Reconstruct(shards)
+	err = encoder.Reconstruct(shards)
 	if err != nil {
 		log.Printf("[recover:%s]datas recover error:%s\n", base58.Encode(description.Id), err.Error())
 		return err
@@ -70,15 +74,21 @@ func (re *RecoverEngine) recoverShard(description *message.TaskDescription) erro
 	return nil
 }
 
-func (re *RecoverEngine) getShard(ctx context.Context, id string, addrs []string, hash []byte, n *int) ([]byte, error) {
+func (re *RecoverEngine) getShard(ctx context.Context, id string, taskID string, addrs []string, hash []byte, n *int) ([]byte, error) {
 	err := re.host.ConnectAddrStrings(id, addrs)
 	if err != nil {
 		return nil, err
 	}
 	stm, err := re.host.NewMsgStream(ctx, id, "/node/0.0.2")
 	if err != nil {
+		if err.Error() == "new stream error:dial to self attempted" {
+			var vhf [32]byte
+			copy(vhf[:], hash)
+			return re.ytfs.Get(common.IndexTableKey(vhf))
+		}
 		return nil, err
 	}
+
 	var msg message.DownloadShardRequest
 	var res message.DownloadShardResponse
 	msg.VHF = hash
@@ -96,7 +106,7 @@ func (re *RecoverEngine) getShard(ctx context.Context, id string, addrs []string
 		return nil, err
 	}
 	*n = *n + 1
-	log.Printf("[recover:%s]get shard success[%d]\n", base58.Encode(hash), *n)
+	log.Printf("[recover:%s]get shard [%s] success[%d]\n", taskID, base58.Encode(hash), *n)
 	return res.Data, nil
 }
 
@@ -118,6 +128,13 @@ func (re *RecoverEngine) HandleMsg(msgData []byte, stm *host.MsgStream) error {
 		return err
 	}
 	log.Printf("[recover:%s]success\n", base58.Encode(res.Id))
+	return nil
+}
+
+func (re *RecoverEngine) PutTask(task []byte) error {
+	if err := re.queue.Put(task); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -167,7 +184,7 @@ func (re *RecoverEngine) HandleMuilteTaskMsg(msgData []byte, stm *host.MsgStream
 
 			// 如果达到提交条件，满个或者已经遍历到数组尾部。提交结果,并清空结果
 			if len(multiTaskOPResults.Id) >= maxResultLen || index >= len(mtdMsg.Tasklist)-1 {
-				re.mutiReplay(&multiTaskOPResults, stm)
+				re.multiReply(&multiTaskOPResults, stm)
 				multiTaskOPResults.Id = make([][]byte, 0)
 				multiTaskOPResults.RES = make([]int32, 0)
 			}
@@ -191,7 +208,7 @@ func (re *RecoverEngine) execCPTask(msgData []byte) *RCTaskMsgResult {
 	var number int
 	// 循环从副本节点获取分片，只要有一个成功就返回
 	for _, v := range msg.Locations {
-		shard, err := re.getShard(ctx, v.NodeId, v.Addrs, msg.DataHash, &number)
+		shard, err := re.getShard(ctx, v.NodeId, base58.Encode(msg.Id), v.Addrs, msg.DataHash, &number)
 		// 如果没有发生错误，分片下载成功，就存储分片
 		if err == nil {
 			var vhf [32]byte
@@ -210,9 +227,9 @@ func (re *RecoverEngine) execCPTask(msgData []byte) *RCTaskMsgResult {
 	return &result
 }
 
-func (re *RecoverEngine) mutiReplay(multiTaskOPResults *message.MultiTaskOpResult, stm *host.MsgStream) {
+func (re *RecoverEngine) multiReply(multiTaskOPResults *message.MultiTaskOpResult, stm *host.MsgStream) {
 
-	defer log.Printf("[recover] result %s, %s\n", util.IDS2String(multiTaskOPResults.Id), multiTaskOPResults.RES)
+	defer log.Printf("[recover] result %s, %v\n", util.IDS2String(multiTaskOPResults.Id), multiTaskOPResults.RES)
 
 	buf, err := proto.Marshal(multiTaskOPResults)
 	if err != nil {
--- recover/recover_test.go
+++ recover/recover_test.go
@@ -13,10 +13,12 @@ import (
 	"testing"
 )
 
-var addr = "/ip4/127.0.0.1/tcp/9001/p2p/16Uiu2HAmCesehUznuW6moZgPAWoJrryXDjbX4gbqn5Zet7f2db2e"
+//var addr = "/ip4/127.0.0.1/tcp/9001/p2p/16Uiu2HAmCesehUznuW6moZgPAWoJrryXDjbX4gbqn5Zet7f2db2e"
+
+var addr = "/ip4/49.233.89.233/tcp/9001/p2p/16Uiu2HAm6DwmkSz3AezYdG5Gkx8xpB2pebpn9Ct3jgpZuurMbXvh"
 
 func TestRecover(t *testing.T) {
-	fi, _ := os.OpenFile("/Users/mac/go/src/github.com/yottachain/YTDataNode/recover/test.dat", os.O_RDONLY, 0644)
+	fi, _ := os.OpenFile("/Users/mac/go/src/github.com/yottachain/YTDataNode/recover/test.data", os.O_RDONLY, 0644)
 	buf, _ := ioutil.ReadAll(fi)
 	h, _ := libp2p.New(context.Background())
 	ma, _ := multiaddr.NewMultiaddr(addr)
