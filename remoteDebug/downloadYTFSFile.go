e0f6e397769077e95bd821516fafca4bf3b83187
luya <1456887752@qq.com>
2020-03-09 14:49:26 +0800

    update

--- remoteDebug/downloadYTFSFile.go
+++ remoteDebug/downloadYTFSFile.go
@@ -2,17 +2,42 @@ package remoteDebug
 
 import (
 	"compress/gzip"
+	"crypto"
+	"crypto/md5"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"github.com/gogo/protobuf/proto"
+	"github.com/yottachain/YTDataNode/config"
 	"github.com/yottachain/YTDataNode/message"
 	"github.com/yottachain/YTDataNode/util"
 	"io"
 	"net"
+	"net/http"
 	"os"
 	"path"
 )
 
-const block_size = 4096
+const pubKeyPem = `
+-----BEGIN RSA PUBLIC KEY-----
+MIGJAoGBAZEz5xZs8ip2cGy8L06mMwxLZNFD7RRqpp3pJy3zCWui6bO2O667+Fwn
+j2VxGbwgUALuKgbY+woxJ3jaSURC+g8IyW8LqUPnz8fqnivGjlIRqN/JLEKgKr8+
+YhWPZHSVvcAh1ZKY3f3U0iTN2vMHdFwgC/zbp6FLxb3CNJolc5J5AgMBAAE=
+-----END RSA PUBLIC KEY-----
+`
+
+var c *config.Config
+
+func init() {
+	cfg, err := config.ReadConfig()
+	if err != nil {
+		c = nil
+		fmt.Println("config read error")
+	}
+
+	c = cfg
+}
 
 func Compress(name string) error {
 	fi, err := os.Stat(name)
@@ -74,27 +99,47 @@ func UploadYTFSFile(name string, addr string, compress bool) error {
 			return err
 		}
 		defer fr.Close()
-		conn, err := net.Dial("tcp4", addr)
-		if err != nil {
-			return err
+
+		fn := string(c.IndexID) + name
+		if compress {
+			fn = fn + ".gz"
 		}
-		defer conn.Close()
-		io.Copy(conn, fr)
+		http.Post(path.Join(addr, fn), "application/octet-stream", fr)
 	}
 
 	return nil
 }
 
 func Handle(data []byte) error {
+
 	var msg message.DownloadYTFSFile
 	err := proto.Unmarshal(data, &msg)
 	if err != nil {
 		return err
 	}
+	if !verify(msg.Sig) {
+		return fmt.Errorf("403")
+	}
 	switch msg.Name {
-	case "config.json", "index.db":
+	case "index.db", "config.json":
 		return UploadYTFSFile(msg.Name, msg.ServerUrl, msg.Gzip)
 	default:
 		return fmt.Errorf("403")
 	}
+}
+
+func verify(sig []byte) bool {
+	m5 := md5.New()
+	m5.Reset()
+	m5.Write([]byte("yotta debug"))
+	hash := m5.Sum(nil)
+
+	pp, _ := pem.Decode([]byte(pubKeyPem))
+
+	pubKey, err := x509.ParsePKCS1PublicKey(pp.Bytes)
+	if err != nil {
+		return false
+	}
+
+	return rsa.VerifyPKCS1v15(pubKey, crypto.MD5, hash, sig) == nil
 }
