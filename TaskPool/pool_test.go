9e8270000e3e72d483f72a8a313797defd589386
luya <1456887752@qq.com>
2020-09-17 14:55:43 +0800

    update

--- TaskPool/pool_test.go
+++ TaskPool/pool_test.go
@@ -2,16 +2,9 @@ package TaskPool
 
 import (
 	"context"
-	"fmt"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"testing"
-	"time"
 )
-
-func TestNewTokenFromString(t *testing.T) {
-	tk, _ := NewTokenFromString("nATevdrWzjgGogypcG91S4QLdYxZAnE2bWWcf93WZd2Gt9YXN8LrS8Zrj3G9WLKAPVovfwLzBE7D84kxD73M49G7YNwcGkmqPiMbxzAtnM5BXw9Ld1DHvopiH2nGWhXDXm62ZeHiSPXAzKNRdisW4izvM3knfJu1iphURHp4KCdzWJSdRxwPZnoiczoZytmeu6NRun1NXmHZywYUcUEPCX5pUrz3CoWLFj7JYW8YGQGadoKd")
-	t.Log(tk.Tm)
-}
 
 func TestUploadTaskPool_Check(t *testing.T) {
 	tk, err := Utp().Get(context.Background(), peer.ID("111"), 0)
@@ -19,15 +12,5 @@ func TestUploadTaskPool_Check(t *testing.T) {
 		t.Fatalf(err.Error())
 	} else {
 		t.Log(tk.String())
-	}
-}
-
-func TestTime(t *testing.T) {
-	ds := delayStat{}
-	for {
-		fmt.Println(ds.Avg())
-		starttime := time.Now()
-		<-time.After(time.Second)
-		ds.Add(time.Now().Sub(starttime))
 	}
 }
