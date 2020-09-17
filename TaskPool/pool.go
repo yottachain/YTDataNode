31fcb5b257a62f9f0eb84be826be25a4eb500b22
luya <1456887752@qq.com>
2020-09-17 17:18:12 +0800

    update

--- TaskPool/pool.go
+++ TaskPool/pool.go
@@ -171,7 +171,9 @@ func (pt *TaskPool) MakeTokenQueue() {
 	if size > 500 {
 		size = 500
 	}
-	pt.tkc.Cancel()
+	if pt.tkc != nil && pt.tkc.Cancel != nil {
+		pt.tkc.Cancel()
+	}
 	pt.tkc = NewTokenQueue(int32(config.Gconfig.MaxToken))
 }
 
