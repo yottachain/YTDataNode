378e7fc8829e7e3ee3a31b0f4333c463f0232f41
luya <1456887752@qq.com>
2020-09-17 15:09:04 +0800

    update

--- TaskPool/tokenQueue.go
+++ TaskPool/tokenQueue.go
@@ -1,14 +1,8 @@
 package TaskPool
 
-import (
-	"container/list"
-	"time"
-)
-
 type TokenQueue struct {
-	Num          int32
-	tc           chan *Token
-	requestQueue *list.List
+	Num int32
+	tc  chan *Token
 }
 
 type getTokenRequest struct {
@@ -20,42 +14,11 @@ func NewTokenQueue(Num int32) *TokenQueue {
 	tq := new(TokenQueue)
 	tq.tc = make(chan *Token, Num)
 	tq.Num = Num
-	tq.requestQueue = list.New()
-	tq.Run()
 	return tq
 }
 
 func (tq *TokenQueue) Get(level int32) chan *Token {
-	gtq := new(getTokenRequest)
-	gtq.Level = level
-	gtq.res = make(chan *Token)
-	if tq.requestQueue.Len() >= int(tq.Num) {
-		gtq.res <- nil
-		return gtq.res
-	}
-	walk := tq.requestQueue.Front()
-	for {
-		if walk.Value.(*getTokenRequest).Level < gtq.Level {
-			break
-		}
-		if walk.Next() != nil {
-			walk = walk.Next()
-		}
-	}
-
-	tq.requestQueue.InsertBefore(gtq, walk)
-	return gtq.res
-}
-
-func (tq *TokenQueue) Run() {
-	for {
-		if tq.requestQueue.Len() > 0 {
-			tk := <-tq.tc
-			gtq := tq.requestQueue.Remove(tq.requestQueue.Front())
-			gtq.(*getTokenRequest).res <- tk
-		}
-		time.Sleep(time.Second)
-	}
+	return tq.tc
 }
 
 func (tq *TokenQueue) Add() {
