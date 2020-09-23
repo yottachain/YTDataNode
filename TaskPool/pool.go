a18dc9300851b0c00998abe130585534e930409c
luya <1456887752@qq.com>
2020-09-23 17:27:37 +0800

    update

--- TaskPool/pool.go
+++ TaskPool/pool.go
@@ -100,8 +100,8 @@ func (pt *TaskPool) FillToken() {
 
 	for {
 		startTime := time.Now()
-		time.Sleep(pt.FillTokenInterval)
-		//time2 := time.Now()
+		<-time.After(pt.FillTokenInterval)
+		time2 := time.Now()
 		d := time.Now().Sub(startTime)
 		c := 0
 		for {
@@ -112,14 +112,14 @@ func (pt *TaskPool) FillToken() {
 				break
 			}
 		}
-		//time3 := time.Now()
-		//fmt.Println("总时长",
-		//	time.Now().Sub(startTime).Milliseconds(),
-		//	"延迟", pt.FillTokenInterval,
-		//	"实际延迟", time2.Sub(startTime).Milliseconds(),
-		//	"入队延迟", time3.Sub(time2).Milliseconds(),
-		//	"实际填充", c,
-		//)
+		time3 := time.Now()
+		fmt.Println("总时长",
+			time.Now().Sub(startTime).Milliseconds(),
+			"延迟", pt.FillTokenInterval,
+			"实际延迟", time2.Sub(startTime).Milliseconds(),
+			"入队延迟", time3.Sub(time2).Milliseconds(),
+			"实际填充", c,
+		)
 
 	}
 }
