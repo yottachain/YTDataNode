567e808a453cb40dcff697a5dcfbc77a8ef8c0c0
luya <1456887752@qq.com>
2020-09-25 16:44:31 +0800

    update

---
+++ TaskPool/requestQueue.go
@@ -0,0 +1,13 @@
+package TaskPool
+
+type request struct {
+	Level int32
+	Res   chan *Token
+}
+
+func NewRequest(level int32) *request {
+	var req = new(request)
+	req.Level = level
+	req.Res = make(chan *Token)
+	return req
+}
