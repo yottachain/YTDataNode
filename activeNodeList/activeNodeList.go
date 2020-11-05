264ebeff5871f65818e08d9a152ebedc63efbf0c
luya <1456887752@qq.com>
2020-11-05 15:00:19 +0800

    update

--- activeNodeList/activeNodeList.go
+++ activeNodeList/activeNodeList.go
@@ -6,12 +6,12 @@ import (
 	"github.com/yottachain/YTDataNode/config"
 	"math/rand"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
 )
 
 var locker = sync.RWMutex{}
-var testconfig = 1
 
 func getUrl() string {
 	var url string
@@ -20,25 +20,8 @@ func getUrl() string {
 	if timeRange == 0 {
 		timeRange = 600
 	}
-	if i < 10 {
-		url = fmt.Sprintf("http://sn0%d.yottachain.net:8082/readable_nodes?timerange=%d", i, timeRange)
-	} else {
-		url = fmt.Sprintf("http://sn%d.yottachain.net:8082/readable_nodes?timerange=%d", i, timeRange)
-	}
-	return url
-}
-
-func getUrl1() string {
-	var url string
-	url = fmt.Sprint("http://139.155.92.246:8082/readable_nodes")
-
-	return url
-}
-
-func getUrl2() string {
-	var url string
-	url = fmt.Sprint("http://172.17.0.4:8082/readable_nodes")
-
+	peerInfo := config.DefaultConfig.BPList[i]
+	url = fmt.Sprintf("http://%s:8082/readable_nodes", strings.Split(peerInfo.Addrs[0], "/")[2])
 	return url
 }
 
@@ -51,26 +34,13 @@ type Data struct {
 }
 
 func Update() {
-	url := getUrl1()
-	//if 1 == testconfig{
-	//	url = getUrl1()
-	//}
-	//
-	//if 2 == testconfig{
-	//	url = getUrl2()
-	//}
+	url := getUrl()
 
 	res, err := http.Get(url)
 	if err != nil {
-			fmt.Println("[recover][nodnlist]can't get active datanode list!")
+		fmt.Println("[recover][nodnlist]can't get active datanode list!")
 		return
 	}
-	//url := getUrl2()
-	//res, err := http.Get(url)
-	//if err != nil{
-	//	//log.Println("[recover][nodnlist]can't get active datanode list!")
-	//	return
-	//}
 
 	dc := json.NewDecoder(res.Body)
 	err = dc.Decode(&nodeList)
