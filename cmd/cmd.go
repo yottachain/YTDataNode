a7413c7653bbe0f61ab7496bd22728add7c2ec87
zhangzhengyan <zzy_19831106@163.com>
2020-12-01 17:21:06 +0800

    publish 1.0.10w

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -106,7 +106,7 @@ func main() {
 	daemonCmd.Flags().BoolVarP(&isDaemon, "d", "d", false, "是否在后台运行")
 
 	RootCommand := &cobra.Command{
-		Version: fmt.Sprintf("%s", "1.0.10v"),
+		Version: fmt.Sprintf("%s", "1.0.10w"),
 		Short:   "ytfs storage node",
 	}
 	RootCommand.AddCommand(initCmd)
--- config/config.go
+++ config/config.go
@@ -391,7 +391,7 @@ func (cfg *Config) PrivKeyString() string {
 }
 
 func (cfg *Config) Version() uint32 {
-	return 158
+	return 159
 }
 
 func Version() uint32 {
