cbac301c00660a09f350a9c46affb8d7983b593a
xiaojianming <xiaojianming@yottachain.io>
2020-12-24 18:12:05 +0800

    debug

--- recover/recover.go
+++ recover/recover.go
@@ -435,9 +435,9 @@ func (re *RecoverEngine) getShard( id string, taskID string, addrs []string, has
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
 	defer cancel()
 
-	log.Println("[recover][debug] getShardcnn A")
+	//log.Println("[recover][debug] getShardcnn A")
 	clt, err := re.sn.Host().ClientStore().GetByAddrString(ctx, id, addrs)
-	log.Println("[recover][debug] getShardcnn B")
+	//log.Println("[recover][debug] getShardcnn B")
 	if err != nil {
 		log.Println("[recover][debug] getShardcnn C err=",err)
 		re.IncFailConn()
@@ -468,9 +468,9 @@ func (re *RecoverEngine) getShard( id string, taskID string, addrs []string, has
 
 	re.IncSuccVersion()
 
-	log.Println("[recover][debug] getShardcnn D")
+	//log.Println("[recover][debug] getShardcnn D")
 	re.GetConShardPass()
-	log.Println("[recover][debug] getShardcnn E")
+	//log.Println("[recover][debug] getShardcnn E")
 	tok,err := re.getRdToken(clt, sw)
 	if err != nil {
 		re.IncFailToken()
