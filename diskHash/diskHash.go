1f55aab59c4024a98617bbcd74a092ddc2372d89
luya <1456887752@qq.com>
2020-12-29 15:49:38 +0800

    update

--- diskHash/diskHash.go
+++ diskHash/diskHash.go
@@ -2,20 +2,43 @@ package diskHash
 
 import (
 	"crypto/md5"
+	"crypto/rand"
 	"encoding/hex"
 	"fmt"
 	"github.com/yottachain/YTDataNode/config"
 	ytfs "github.com/yottachain/YTFS"
+	"github.com/yottachain/YTFS/common"
 	"os"
 )
 
 const CheckBlockSize = 64 + 16*1024
 
+func randShard(n int) map[common.IndexTableKey][]byte {
+	var res = make(map[common.IndexTableKey][]byte, n)
+	for i := 0; i < 5; i++ {
+		buf := make([]byte, 16)
+		rand.Read(buf)
+		key := md5.Sum(buf)
+		res[key] = buf
+	}
+	return res
+}
+func randWrite(ytfs *ytfs.YTFS) error {
+	_, err := ytfs.BatchPut(randShard(5))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func GetHash(ytfs *ytfs.YTFS) (string, error) {
 	cfg := config.DefaultConfig
 	l := ytfs.Len()
 	if l < 5 {
-		return "", fmt.Errorf("ytfs len < 5")
+		err := randWrite(ytfs)
+		if err != nil {
+			return "", fmt.Errorf("write ytfs checkData error")
+		}
 	}
 
 	s1 := cfg.Storages[0]
