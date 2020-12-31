8084d69a40c974f457ae70fe63f8574e09f8d780
luya <1456887752@qq.com>
2020-12-31 17:20:01 +0800

    update

--- randDownload/randDownload.go
+++ randDownload/randDownload.go
@@ -138,7 +138,9 @@ func Run() {
 
 				err := DownloadFromRandNode()
 				if err != nil {
-					log.Println("[randDownload]error", err.Error())
+					if config.IsDev {
+						log.Println("[randDownload]error", err.Error())
+					}
 					atomic.AddUint64(&errorCount, 1)
 				} else {
 					atomic.AddUint64(&successCount, 1)
