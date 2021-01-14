f2b007077ae0e3e1d9bd2dbbde397d8d773091e4
luya <1456887752@qq.com>
2021-01-14 18:09:44 +0800

    update

--- statistics/counter.go
+++ statistics/counter.go
@@ -15,7 +15,11 @@ func (rc *RateCounter) AddCount() {
 	atomic.AddInt64(&rc.Count, 1)
 }
 func (rc *RateCounter) AddSuccess() {
-	atomic.AddInt64(&rc.Success, 1)
+	count := atomic.LoadInt64(&rc.Count)
+	success := atomic.LoadInt64(&rc.Success)
+	if success < count {
+		atomic.AddInt64(&rc.Success, 1)
+	}
 }
 func (rc *RateCounter) Reset() {
 	atomic.StoreInt64(&rc.Count, 0)
