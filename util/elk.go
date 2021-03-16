a1cf59aa63ae1c1defc504c74147e6d3b03ab48c
luya <1456887752@qq.com>
2021-03-16 10:50:07 +0800

    update

--- util/elk.go
+++ util/elk.go
@@ -1,34 +1,31 @@
 package util
 
-//
-//import (
-//	"fmt"
-//	"github.com/elastic/go-elasticsearch/v8"
-//	"github.com/yottachain/YTDataNode/config"
-//	"github.com/yottachain/YTElkProducer"
-//	"github.com/yottachain/YTElkProducer/conf"
-//)
-//
-//func NewElkClient(tbstr string) YTElkProducer.Client {
-//	elkConf := elasticsearch.Config{
-//		Addresses: []string{"https://es-kljdi4to.public.tencentelasticsearch.com:9200"},
-//		Username:  "elastic",
-//		Password:  "yotta@2021",
-//	}
-//
-//	ytESConfig := conf.YTESConfig{
-//		ESConf:      elkConf,
-//		DebugMode:   true,
-//		IndexPrefix: tbstr,
-//		IndexType:   "log",
-//	}
-//
-//	if config.Gconfig.ElkReport2 {
-//		client, err := YTElkProducer.NewClient(ytESConfig)
-//		if err != nil {
-//			fmt.Println(err)
-//		}
-//		return client
-//	}
-//	return nil
-//}
+import (
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/yottachain/YTElkProducer"
+	"github.com/yottachain/YTElkProducer/conf"
+)
+
+func NewElkClient(tbstr string, open *bool) YTElkProducer.Client {
+	elkConf := elasticsearch.Config{
+		Addresses: []string{"https://es-hxwjwkzw.public.tencentelasticsearch.com:9200"},
+		Username:  "elastic",
+		Password:  "yotta@2021",
+	}
+
+	ytESConfig := conf.YTESConfig{
+		ESConf:      elkConf,
+		DebugMode:   true,
+		IndexPrefix: tbstr,
+		IndexType:   "log",
+	}
+
+	if *open {
+		client, _ := YTElkProducer.NewClient(ytESConfig)
+		//if err != nil {
+		//	fmt.Println("error:", err)
+		//}
+		return client
+	}
+	return nil
+}
