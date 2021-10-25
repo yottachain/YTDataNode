e3388a0048413cba2dbcd2f7c218eeabe98e6d9d
zhangzhengyan <zzy_19831106@163.com>
2021-10-25 14:54:31 +0800

    update

--- capProof/proof.go
+++ capProof/proof.go
@@ -4,6 +4,12 @@ import (
 	Ytfs "github.com/yottachain/YTFS"
 )
 
-func GetCapProofSpace(ytfs *Ytfs.YTFS) uint32 {
-	return ytfs.GetCapProofSpace()
+func GetCapProofSpace(ytfs *Ytfs.YTFS) (realCap uint32) {
+	availableShards := ytfs.GetCapProofSpace()
+	if (availableShards % 65536) != 0 {
+		realCap = (availableShards / 65536 + 1) * 65536
+	}else {
+		realCap = availableShards
+	}
+	return
 }
