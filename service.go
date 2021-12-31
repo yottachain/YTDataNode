1ab6fbb78ad8ce20ed9b923117099ec64ba16e42
zhangzhengyan <zzy_19831106@163.com>
2021-12-31 22:09:18 +0800

    update

--- service.go
+++ service.go
@@ -10,7 +10,6 @@ import (
 	"github.com/yottachain/YTDataNode/capProof"
 	"github.com/yottachain/YTDataNode/config"
 	"github.com/yottachain/YTDataNode/diskHash"
-	"github.com/yottachain/YTDataNode/magrate"
 	"github.com/yottachain/YTDataNode/randDownload"
 	"github.com/yottachain/YTDataNode/setRLimit"
 	"github.com/yottachain/YTDataNode/slicecompare"
@@ -59,15 +58,15 @@ func (sn *storageNode) Service() {
 	go capProof.TimerRun(sn.ytfs)
 
 	//消息注册前 启动gc clean and magrate data
-	go func() {
-		stopUp = true
-		(&gc.GcWorker{sn}).CleanGc()
-		err := magrate.NewMr().Run(sn.ytfs, sn.config.UseKvDb, sn.config.IndexID)
-		if err != nil {
-			log.Printf("%s\n", err.Error())
-		}
-		stopUp = false
-	}()
+	//go func() {
+	//	stopUp = true
+	//	(&gc.GcWorker{sn}).CleanGc()
+	//	err := magrate.NewMr().Run(sn.ytfs, sn.config.UseKvDb, sn.config.IndexID)
+	//	if err != nil {
+	//		log.Printf("%s\n", err.Error())
+	//	}
+	//	stopUp = false
+	//}()
 
 
 	// 初始化统计
