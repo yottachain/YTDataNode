3e065c2c5b7c13b1ce7a382df2a4660f89f8e526
zhangzhengyan <zzy_19831106@163.com>
2023-02-13 20:42:09 +0800

    statistics/waticount.go  fix bug

--- config/config.go
+++ config/config.go
@@ -1,27 +1,27 @@
 package config
 
 import (
-    "bytes"
-    "crypto/md5"
-    "encoding/json"
-    "fmt"
+	"bytes"
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
 
-    "io/ioutil"
-    "log"
-    "os"
-    "path"
-    "runtime"
-    "strings"
-    "time"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"runtime"
+	"strings"
+	"time"
 
-    "github.com/eoscanada/eos-go/btcsuite/btcutil/base58"
-    "github.com/libp2p/go-libp2p-core/peer"
-    "github.com/multiformats/go-multiaddr"
-    "github.com/spf13/viper"
+	"github.com/eoscanada/eos-go/btcsuite/btcutil/base58"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/multiformats/go-multiaddr"
+	"github.com/spf13/viper"
 
-    ci "github.com/libp2p/go-libp2p-core/crypto"
-    "github.com/yottachain/YTDataNode/util"
-    ytfsOpts "github.com/yottachain/YTFS/opt"
+	ci "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/yottachain/YTDataNode/util"
+	ytfsOpts "github.com/yottachain/YTFS/opt"
 )
 
 type peerInfo struct {
@@ -63,7 +63,7 @@ type Config struct {
 	MaxConn       int           `json:"MaxConn"`
 	TokenInterval time.Duration `json:"TokenInterval"`
 	*ytfsOpts.Options
-	UpdateURL string `json:"update_url"`
+	UpdateURL    string `json:"update_url"`
 	bpListMd5    []byte
 	DisableWrite bool
 	AllocSpace   uint64
@@ -89,21 +89,21 @@ func DefaultYTFSOptions() *ytfsOpts.Options {
 	return opts
 }
 
-func InitRowsCols(size uint64, n uint32, db string)(uint64, uint64, error){
+func InitRowsCols(size uint64, n uint32, db string) (uint64, uint64, error) {
 	var d uint32 = 1 << 14
 	var m uint64
 
 	//expendRatioM = 1.2 = 12 / 10
 	m = size / uint64(d) / uint64(n)
-	if db == "rocksdb"{
+	if db == "rocksdb" {
 		return m, uint64(n), nil
 	}
 
-	fmt.Println("InitRowsCols,M=",m)
+	fmt.Println("InitRowsCols,M=", m)
 	// IndexTableCols(M) = m * expendRatioM,  expendRatioM = 1.2  (512 <= IndexTableCols(M) <= 2048)
-	if m < 420 || m > 1700{
-		err := fmt.Errorf("IndexTableCols not suitable,M=",m)
-		fmt.Println("[error]init failed, IndexTableCols not suitable,M=",m)
+	if m < 420 || m > 1700 {
+		err := fmt.Errorf("IndexTableCols not suitable,M=", m)
+		fmt.Println("[error]init failed, IndexTableCols not suitable,M=", m)
 		return 0, 0, err
 	}
 
@@ -112,44 +112,44 @@ func InitRowsCols(size uint64, n uint32, db string)(uint64, uint64, error){
 
 // GetYTFSOptionsByParams 通过参数生成YTFS配置
 func GetYTFSOptionsByParams(size uint64, m uint32) *ytfsOpts.Options {
-    yp := util.GetYTFSPath()
-    var d uint32 = 16384
-    n := size / uint64(d) / uint64(m)
+	yp := util.GetYTFSPath()
+	var d uint32 = 16384
+	n := size / uint64(d) / uint64(m)
 
-    for {
-        if (n & (n - 1)) == 0 {
-            break
-        }
-        n = n + 1
-    }
+	for {
+		if (n & (n - 1)) == 0 {
+			break
+		}
+		n = n + 1
+	}
 	m = uint32(size / uint64(d) / n)
 
-    opts := &ytfsOpts.Options{
-        YTFSTag: "ytfs",
-        Storages: []ytfsOpts.StorageOptions{
-            {
-                StorageName:   path.Join(yp, "storage"),
-                StorageType:   0,
-                ReadOnly:      false,
-                SyncPeriod:    1,
-                StorageVolume: size,
-                DataBlockSize: 16384,
-            },
-        },
-        ReadOnly:       false,
-        SyncPeriod:     1,
-        IndexTableCols: m,
-        IndexTableRows: uint32(n),
-        DataBlockSize:  d,
-        TotalVolumn:    size,
-        UseKvDb:        true,
-    }
+	opts := &ytfsOpts.Options{
+		YTFSTag: "ytfs",
+		Storages: []ytfsOpts.StorageOptions{
+			{
+				StorageName:   path.Join(yp, "storage"),
+				StorageType:   0,
+				ReadOnly:      false,
+				SyncPeriod:    1,
+				StorageVolume: size,
+				DataBlockSize: 16384,
+			},
+		},
+		ReadOnly:       false,
+		SyncPeriod:     1,
+		IndexTableCols: m,
+		IndexTableRows: uint32(n),
+		DataBlockSize:  d,
+		TotalVolumn:    size,
+		UseKvDb:        true,
+	}
 
-    if runtime.GOOS == "windows" {
-        opts.UseKvDb = false
-    }
+	if runtime.GOOS == "windows" {
+		opts.UseKvDb = false
+	}
 
-    return opts
+	return opts
 }
 
 // GetYTFSOptionsByParams2 通过参数生成YTFS配置, 多storage配置
@@ -237,94 +237,94 @@ func getBPList() []peerInfo {
 		return bplist
 	}
 
-//	jsdata := `
-//    [
-//    {
-//      "ID": "16Uiu2HAm7o24DSgWTrcu5sLCgSkf3D3DQqzpMz9W1Bi7F2Cc4SF6",
-//      "Addrs": ["/dns4/sn00.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmNe1bZF2s7msxqy9tFT7WDfUaJa98h1KBhAmTTHvcZqpA",
-//      "Addrs": ["/dns4/sn01.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAkyZAuzcjmpFhk1pCLAZaYusV3wXmrEhnnNDfeJjkVoQc6",
-//      "Addrs": ["/dns4/sn02.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmSFq7SbwcfYVn3NzWuuV7SizQEVjKEwty1knZuzTA7jDq",
-//      "Addrs": ["/dns4/sn03.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmC7DSN4kNi64sB5N9aMgv9DjTTrtydf4YKS3Q56hYsDNS",
-//      "Addrs": ["/dns4/sn04.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmSFgs5Pj6hFdAzCAvFGH78ew7egakT6VqL1xaLdvxnnSc",
-//      "Addrs": ["/dns4/sn05.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmP2RuNAkXdtQDiFqVuBA8yERh91JV6b29rQpAGKkb3PiM",
-//      "Addrs": ["/dns4/sn06.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmJQ7cjPzi7u4NdgYK7xWqDgEVBqAqNPrmxY2KVKGpND2W",
-//      "Addrs": ["/dns4/sn07.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmDKteRvgXPtzz3pvhGn56HH7uo8WqoGqJWPArY4G1kuWP",
-//      "Addrs": ["/dns4/sn08.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmDpQ6527dqtiv5fixTptQBtGa561BZeUTDuALiAZwQNGR",
-//      "Addrs": ["/dns4/sn09.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmATZsCop9hkKDbmtyLbizLQU92jrCVpWvzRChKRQbwzy7",
-//      "Addrs": ["/dns4/sn10.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmGheSFhwbpihhEnyZUxsVr6Rn9z5v2XDMeEyAfK2K4nwG",
-//      "Addrs": ["/dns4/sn11.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAm2JANbeeaXDa9JaDTU5Q1h2hmjJGJx91LpYd36pdoDWdx",
-//      "Addrs": ["/dns4/sn12.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAm3Cmzqg9TKR6FvEH5NSgzLZgDZb4xtPC9aYhqbc9p7WM5",
-//      "Addrs": ["/dns4/sn13.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAm2CzizQh2AU8NXK5z2bvJUaFuPiM9Z6R1uDEFKDvob4mJ",
-//      "Addrs": ["/dns4/sn14.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmTd1jqEGLThwcrD9yYG1JsHHj7qsDJDBcdgMLMvaBnksU",
-//      "Addrs": ["/dns4/sn15.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmHufUv4udcL1f1bNP4r6VqDBppmKH495iQKSgv6nWGoZA",
-//      "Addrs": ["/dns4/sn16.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmKS35S4JQk8BDUvgWhjGLMJ1f9zWJhT3QeRRyFdReXeue",
-//      "Addrs": ["/dns4/sn17.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmUyPbR4wcKtGi6n84CGkHsXsHZZ2sGrnhJPAqJmFCMfDW",
-//      "Addrs": ["/dns4/sn18.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmLUCp92e25HXiZW8fMwpCUfhQRcNGL7PibTDtg51JTRCq",
-//      "Addrs": ["/dns4/sn19.yottachain.net/tcp/9999"]
-//    },
-//    {
-//      "ID": "16Uiu2HAmBG1d8HHBApLg9MrDqgUX4LoKcFCSCrq54QW3mkqRheo1",
-//      "Addrs": ["/dns4/sn20.yottachain.net/tcp/9999"]
-//    }
-//  ]
-//`
+	//	jsdata := `
+	//    [
+	//    {
+	//      "ID": "16Uiu2HAm7o24DSgWTrcu5sLCgSkf3D3DQqzpMz9W1Bi7F2Cc4SF6",
+	//      "Addrs": ["/dns4/sn00.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmNe1bZF2s7msxqy9tFT7WDfUaJa98h1KBhAmTTHvcZqpA",
+	//      "Addrs": ["/dns4/sn01.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAkyZAuzcjmpFhk1pCLAZaYusV3wXmrEhnnNDfeJjkVoQc6",
+	//      "Addrs": ["/dns4/sn02.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmSFq7SbwcfYVn3NzWuuV7SizQEVjKEwty1knZuzTA7jDq",
+	//      "Addrs": ["/dns4/sn03.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmC7DSN4kNi64sB5N9aMgv9DjTTrtydf4YKS3Q56hYsDNS",
+	//      "Addrs": ["/dns4/sn04.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmSFgs5Pj6hFdAzCAvFGH78ew7egakT6VqL1xaLdvxnnSc",
+	//      "Addrs": ["/dns4/sn05.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmP2RuNAkXdtQDiFqVuBA8yERh91JV6b29rQpAGKkb3PiM",
+	//      "Addrs": ["/dns4/sn06.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmJQ7cjPzi7u4NdgYK7xWqDgEVBqAqNPrmxY2KVKGpND2W",
+	//      "Addrs": ["/dns4/sn07.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmDKteRvgXPtzz3pvhGn56HH7uo8WqoGqJWPArY4G1kuWP",
+	//      "Addrs": ["/dns4/sn08.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmDpQ6527dqtiv5fixTptQBtGa561BZeUTDuALiAZwQNGR",
+	//      "Addrs": ["/dns4/sn09.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmATZsCop9hkKDbmtyLbizLQU92jrCVpWvzRChKRQbwzy7",
+	//      "Addrs": ["/dns4/sn10.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmGheSFhwbpihhEnyZUxsVr6Rn9z5v2XDMeEyAfK2K4nwG",
+	//      "Addrs": ["/dns4/sn11.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAm2JANbeeaXDa9JaDTU5Q1h2hmjJGJx91LpYd36pdoDWdx",
+	//      "Addrs": ["/dns4/sn12.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAm3Cmzqg9TKR6FvEH5NSgzLZgDZb4xtPC9aYhqbc9p7WM5",
+	//      "Addrs": ["/dns4/sn13.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAm2CzizQh2AU8NXK5z2bvJUaFuPiM9Z6R1uDEFKDvob4mJ",
+	//      "Addrs": ["/dns4/sn14.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmTd1jqEGLThwcrD9yYG1JsHHj7qsDJDBcdgMLMvaBnksU",
+	//      "Addrs": ["/dns4/sn15.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmHufUv4udcL1f1bNP4r6VqDBppmKH495iQKSgv6nWGoZA",
+	//      "Addrs": ["/dns4/sn16.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmKS35S4JQk8BDUvgWhjGLMJ1f9zWJhT3QeRRyFdReXeue",
+	//      "Addrs": ["/dns4/sn17.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmUyPbR4wcKtGi6n84CGkHsXsHZZ2sGrnhJPAqJmFCMfDW",
+	//      "Addrs": ["/dns4/sn18.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmLUCp92e25HXiZW8fMwpCUfhQRcNGL7PibTDtg51JTRCq",
+	//      "Addrs": ["/dns4/sn19.yottachain.net/tcp/9999"]
+	//    },
+	//    {
+	//      "ID": "16Uiu2HAmBG1d8HHBApLg9MrDqgUX4LoKcFCSCrq54QW3mkqRheo1",
+	//      "Addrs": ["/dns4/sn20.yottachain.net/tcp/9999"]
+	//    }
+	//  ]
+	//`
 
 	jsdata := `
     [
@@ -436,7 +436,7 @@ func (cfg *Config) PrivKeyString() string {
 }
 
 func (cfg *Config) Version() uint32 {
-	return 286
+	return 287
 }
 
 func Version() uint32 {
