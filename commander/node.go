a091713222a5ff5591bf62b8c53043f5a5e9df18
zhangzhengyan <zzy_19831106@163.com>
2023-07-02 02:27:53 +0800

    YTFS: IgnoreStorageHeaderErr = false

--- commander/node.go
+++ commander/node.go
@@ -87,18 +87,6 @@ func NewID() (string, int) {
 
 func setLimits() {
 	if runtime.GOOS == "linux" {
-		//		cmd := exec.Command("bash", "-c", "ulimit -n 60000")
-		//		stdin, _ := cmd.StdinPipe()
-		//		_, _ = fmt.Fprint(stdin, `
-		//echo "* soft nofile 655350" > /etc/security/limits.conf
-		//echo "* hard nofile 655350" >> /etc/security/limits.conf
-		//echo "* soft nproc 655350" >> /etc/security/limits.conf
-		//echo "* hard nproc 655350" >> /etc/security/limits.conf
-		//echo "* soft core unlimited" >> /etc/security/limits.conf
-		//echo "* hard core unlimited" >> /etc/security/limits.conf
-		//`)
-		//		_, _ = cmd.Output()
-
 		var rLimit syscall.Rlimit
 		err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
 		if err != nil {
